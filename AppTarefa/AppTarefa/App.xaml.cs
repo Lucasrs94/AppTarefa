b788355a47bcb578460fda996af3d370787d173a
Lucas <lucasbsil_rs@hotmail.com>
2018-12-26 21:57:35 -0300

    tela de cadastro

--- AppTarefa/AppTarefa/App.xaml.cs
+++ AppTarefa/AppTarefa/App.xaml.cs
@@ -11,7 +11,7 @@ namespace AppTarefa
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new AppTarefa.Tela.Inicioxaml());
         }
 
         protected override void OnStart()
---
+++ AppTarefa/AppTarefa/Tela/Inicioxaml.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTarefa.Tela
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Inicioxaml : ContentPage
+	{
+		public Inicioxaml ()
+		{
+			InitializeComponent ();
+		}
+        private void ActionCadastro(object sender, EventArgs args)
+        {
+            // Chamo a tela cadastro
+            Navigation.PushAsync(new Cadastro());
+        }
+
+
+    }
+}
